e0839d8090eb57d6d27b7b8b63a80373e91dacbf
franklyn255 <franklinakunne025@gmail.com>
2023-02-16 12:44:35 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is a function
+ *
+ * return : ends the main function
+ */
+
+main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
