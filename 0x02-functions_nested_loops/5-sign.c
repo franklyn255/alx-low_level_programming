f5477a0c5b415f96f8ca5b5ab61e0966f8e24f5e
franklyn255 <franklinakunne025@gmail.com>
2023-02-21 12:18:58 -0800

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs
+ *
+ * @n: value entered by the user
+ *
+ * Return: returns numbers
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
