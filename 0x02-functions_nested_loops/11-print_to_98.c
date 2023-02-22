d6d874909c577f8d5685e7a57df32587799f2c0b
franklyn255 <franklinakunne025@gmail.com>
2023-02-22 01:31:59 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,10 +29,10 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n; i <= 98; i--)
+		for (i = n; i >= 98; i--)
 			printf("%d, ", i);
 				printf("\n");
 	}
-	else if (n == 98)
+	else
 		printf("%d, ", n);
 }
