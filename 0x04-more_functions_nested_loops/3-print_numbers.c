9311e57844f4dc7519984df3305a213c77ba3d12
franklyn255 <franklinakunne025@gmail.com>
2023-02-23 15:23:50 -0800

    prints 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers from 0-9
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		_putchar(i + '0');
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
