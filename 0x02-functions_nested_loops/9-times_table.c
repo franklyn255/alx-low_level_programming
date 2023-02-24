545ea844ba9817541c848ffbd0388a1e37432d52
franklyn255 <franklinakunne025@gmail.com>
2023-02-23 16:50:42 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,30 +12,30 @@ void times_table(void)
 	int i, j, k;
 
 	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
 		{
-			for (j = 0; j < 10; j++)
-			{
-				k = i * j;
+			k = i * j;
 
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k <= 9 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k > 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(k/10 + '0');
-					_putchar(k%10 + '0');
-				}
+			if (j == 0)
+			{
+				_putchar(k + '0');
 			}
-			_putchar('\n');
+			else if (k <= 9 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
 		}
+		_putchar('\n');
+	}
 }
