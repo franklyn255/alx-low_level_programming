f3578c8d9b0a785645e66592aff2a34a3c3e7647
franklyn255 <franklinakunne025@gmail.com>
2023-02-27 04:58:43 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value to 98
+ * @n: parameter for updating value to 98
+ * Return: always n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void reset_to_98(int *n);
+#include <stdio.h>
+int _putchar(char c);
+
+
+#endif 
