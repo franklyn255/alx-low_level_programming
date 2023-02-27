846ccca9d03de71702a8ae631cb5a64c0de23cda
franklyn255 <franklinakunne025@gmail.com>
2023-02-27 13:44:13 -0800

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap th value of two integers
+ * @a: integer 1
+ * @b: integer 2
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a =  42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #include <stdio.h>
 int _putchar(char c);
 
