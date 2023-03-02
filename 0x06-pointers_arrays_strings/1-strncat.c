9bef08a0f5183ef60cfe0b6e5bbbc7ada301d7f2
franklyn255 <franklinakunne025@gmail.com>
2023-03-02 15:20:41 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 		destlen++;
 	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		dest[destlen + i] = src[i];
 	return (dest);
 }
