1bc4e752b0366dcb660018f5c81b8178f389424c
franklyn255 <franklinakunne025@gmail.com>
2023-03-03 13:39:36 -0800

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,19 +9,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
 	int j;
-	int k = 0;
 
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[j];
-		k++;
+		dest[j] = src[j];
 		j++;
 	}
-        while (dest[i] != '\0')
-                i++;
-	dest[i] = '\0';
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }
