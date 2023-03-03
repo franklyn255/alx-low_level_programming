134861c7351a0f1fb8eada6c824654bb41ccd7f6
franklyn255 <franklinakunne025@gmail.com>
2023-03-03 08:58:25 -0800

    reverses string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,11 @@ void reverse_array(int *a, int n)
 	int i;
 	int temp;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n--; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = temp;
+		a[i] = a[n];
+		a[n] = temp;
 	}
 
 }
