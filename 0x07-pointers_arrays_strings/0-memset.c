0fb1e58481ae92ee6181180aa1fb5881b631e904
franklyn255 <franklinakunne025@gmail.com>
2023-03-06 09:02:31 -0800

    the number of bytes to be written to the memory block

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,19 @@
 #include "main.h"
+/**
+ * _memset - a function that fills memory with a constant byte.
+ * @s: a pointer to the memory block to be filled.
+ * @b: the byte value to be written to the memory block.
+ * @n: the number of bytes to be written to the memory block.
+ * Return: returns pointer
+ */
 
-int 
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
