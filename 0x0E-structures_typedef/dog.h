09e322a0da382066e374d2abdecad694f1ee697d
franklyn255 <franklinakunne025@gmail.com>
2023-03-21 09:01:08 -0700

    Define a new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - create variables and stores information
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
