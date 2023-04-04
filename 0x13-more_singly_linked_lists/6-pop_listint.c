69b04c502eb82d6ea116a2d315eba99706114cc2
franklyn255 <franklinakunne025@gmail.com>
2023-04-04 14:18:20 -0700

    deletes first node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * pop_listint - returns listint_t data
+ * @head: pointer to listint_t list
+ * Return: node data
+ */
+
 int pop_listint(listint_t **head)
 {
-	listint_t *temp, *del;
+	listint_t *temp;
+	int dig;
 
 	if (*head == NULL)
 		return (0);
-	del = *head;
-	*head = (*head)->next;
-	free(del);
-	while (*head != NULL)
-	{
-		temp = *head;
-		*head = (*head)->next;
-		printf("%d\n", temp->n);
-	}
-	return (temp->n);
+	dig = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (dig);
 }
