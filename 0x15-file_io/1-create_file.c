21170811794b85d679a79caf028d4483c8eb250f
franklyn255 <franklinakunne025@gmail.com>
2023-04-08 03:02:51 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: pointer to file
+ * @text_content: pointer to a string to write to th file
+ * Return: 1 on success or -1 if function fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int w, fd, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	w = write (fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
