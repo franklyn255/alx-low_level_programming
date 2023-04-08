46d458d84638f84beb6f3eb5dd58b81401c96fdc
franklyn255 <franklinakunne025@gmail.com>
2023-04-08 03:37:06 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int w, fd, len;
+        int w, fd, len;
 
-	if (filename == NULL)
-		return (-1);
+        if (filename == NULL)
+                return (-1);
 
-	if (text_content != NULL)
-	{
-		len = 0;
-		while (text_content[len])
-			len++;
-	}
+        if (text_content != NULL)
+        {
+                len = 0;
+                while (text_content[len])
+                        len++;
+        }
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0666);
+        w = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
-		return (-1);
-	close(fd);
-	return (1);
+        if (fd == -1 || w == -1)
+                return (-1);
+        close(fd);
+        return (1);
 }
